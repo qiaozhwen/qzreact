7da3749ffc099e042d3b72c47a8627589d224a7c
johnson.qiao <johnson.qiao@derbysoft.net>
2020-03-27 10:26:38 +0800

    0327

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -5,7 +5,10 @@ export default class App extends Component{
         return(
             <div className='box'>
                 <div className='box-image'>
-                    <img src={require('../assests/images/America.png')}/>
+                    <img src={require('../assests/images/react.png')}/>
+                </div>
+                <div style={{color: 'white',width: '100%', textAlign:'center'}}>
+                    <span>Welcome to your First React App, <a href='https://reactjs.org/'>Learn More</a></span>
                 </div>
             </div>
         )
